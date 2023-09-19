19b9b28b761167e3b2cdad22c55bee0136885cee
HKChiuy <144956998+HKChiuy@users.noreply.github.com>
2023-09-19 15:42:38 -0500

    Create StartScreen.java

---
+++ StartScreen.java
@@ -0,0 +1,13 @@
+import mayflower.*;
+
+public class StartScreen extends World{
+    
+    public StartScreen(){
+        
+        //setBackground("img");
+    }
+    
+    public void act(){
+        
+    }
+}
