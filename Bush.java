4447e584c51a7d3535234cfd92adf8f53936588a
Sitanshu Singh <99071964+singhsitanshu@users.noreply.github.com>
2023-09-19 16:13:58 -0500

    Add files via upload

---
+++ Bush.java
@@ -0,0 +1,13 @@
+import mayflower.*;
+
+public class Bush extends Actor
+{
+    
+    public Bush() {
+        setImage("img/Object/Bush (1).png"); 
+    }
+    
+    public void act(){
+    }
+    
+}
