d3c41df0a549d732b7fb4dde6c5823acd9433faa
HKChiuy <144956998+HKChiuy@users.noreply.github.com>
2023-09-19 15:43:31 -0500

    Create GameLostScreen.java

---
+++ GameLostScreen.java
@@ -0,0 +1,13 @@
+import mayflower.*;
+
+public class GameLostScreen extends World{
+    
+    public GameLostScreen(){
+     
+        //setBackground("img");
+    }
+    
+    public void act(){
+        
+    }
+}
