a412bc86208a8773f2b5cacb3a880610798152b5
Sitanshu Singh <99071964+singhsitanshu@users.noreply.github.com>
2023-09-19 16:11:58 -0500

    ladder

---
+++ Ladder.java
@@ -0,0 +1,17 @@
+import mayflower.*;
+/**
+ * Write a description of class Ladder here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ladder extends Actor
+{
+    public Ladder() {
+        setImage("img/Object/ladder.png");
+        scale(10, 10);
+    }
+    
+    public void act(){
+    }
+}
