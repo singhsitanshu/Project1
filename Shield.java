1ae4cd44bf2dfe20b09c9816ef2244292a3d7b55
HKChiuy <144956998+HKChiuy@users.noreply.github.com>
2023-09-21 16:13:47 -0500

    Create Shield.java

---
+++ Shield.java
@@ -0,0 +1,24 @@
+import mayflower.*;
+
+
+public class Shield extends Actor
+{
+    public Shield()
+    {
+        setImage("img/Object/crate.png");
+    }
+    
+    public void act(){
+        
+        if (isTouching(Cat.class)){
+        
+            Object a = getOneIntersectingObject(Cat.class);
+            Cat c = (Cat) a;
+            
+            World w = getWorld();
+            w.removeObject(this);
+            
+            c.setHasShield(true);
+        }
+    }
+}
