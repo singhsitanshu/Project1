803cc2378347dad0c1b6f6f0db2983b434dc4020
bbgolden <144957630+bbgolden@users.noreply.github.com>
2023-09-21 16:19:00 -0500

    Add files via upload
    lvl2 files

---
+++ DMGWater.java
@@ -0,0 +1,15 @@
+import mayflower.*;
+
+public class DMGWater extends Actor
+{
+    
+    public DMGWater() {
+        MayflowerImage img = new MayflowerImage("img/Tiles/DMGWater.png");
+        img.scale(100, 100);
+        setImage(img);
+    }
+    
+    public void act() {
+    }
+    
+}
---
+++ FloorWater.java
@@ -0,0 +1,14 @@
+import mayflower.*;
+import mayflower.*;
+
+public class FloorWater extends Actor
+{
+    public FloorWater() {
+        MayflowerImage img = new MayflowerImage("img/Tiles/FloorWater.png");
+        img.scale(100, 100);
+        setImage(img);
+    }
+    
+    public void act() {
+    }
+}
