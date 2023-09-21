17169f2925684562bb309865c6adeb154fbb42ce
HKChiuy <144956998+HKChiuy@users.noreply.github.com>
2023-09-21 15:16:46 -0500

    Update StartScreen.java

--- StartScreen.java
+++ StartScreen.java
@@ -1,13 +1,22 @@
 import mayflower.*;
 
 public class StartScreen extends World{
+    private boolean isGameStarted;
     
     public StartScreen(){
         
-        //setBackground("img");
+        setBackground("img/BG/startscreen.png");
+        isGameStarted = false;
     }
     
     public void act(){
         
+        if (!isGameStarted && Mayflower.isKeyDown(Keyboard.KEY_ENTER)){
+            
+            isGameStarted = true;
+        }
+        
+        if (isGameStarted)
+            Mayflower.setWorld(new Level1());
     }
 }
