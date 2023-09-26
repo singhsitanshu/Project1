1255140a613f93c197c78eb3c250d66e74590f8f
Sitanshu Singh <99071964+singhsitanshu@users.noreply.github.com>
2023-09-26 16:15:11 -0500

    Add files via upload

---
+++ Barrier.java
@@ -0,0 +1,49 @@
+import mayflower.*;
+/**
+ * Write a description of class Dog here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Barrier extends Actor
+{
+    public Barrier()
+    {
+        setImage("img/barrier.png");
+    }
+    
+    public void act()
+    {
+        if (isTouching(Cat.class))
+        {
+            Object a = getOneIntersectingObject(Cat.class);
+            Cat c = (Cat)a;     
+            World w = getWorld();
+            w.removeObject(c);
+        }
+        
+        if (isTouching(Block.class))
+        {
+            Object a = getOneIntersectingObject(Block.class);
+            Block c = (Block)a;     
+            World w = getWorld();
+            w.removeObject(c);
+        }
+        
+        if (isTouching(Ladder.class))
+        {
+            Object a = getOneIntersectingObject(Ladder.class);
+            Ladder c = (Ladder)a;     
+            World w = getWorld();
+            w.removeObject(c);
+        }
+        
+        if (isTouching(Spike.class))
+        {
+            Object a = getOneIntersectingObject(Spike.class);
+            Spike c = (Spike)a;     
+            World w = getWorld();
+            w.removeObject(c);
+        }
+    }
+}
---
+++ Coin.java
@@ -0,0 +1,14 @@
+import mayflower.*;
+/**
+ * Write a description of class Coin here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Coin extends MoveableAnimatedActor
+{
+    public Coin()
+    {
+        
+    }
+}
