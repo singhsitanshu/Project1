36d939c0465e7d0d7e5705aab311d5deb1985833
Sitanshu Singh <99071964+singhsitanshu@users.noreply.github.com>
2023-09-28 16:10:47 -0500

    Add files via upload

--- Coin.java
+++ Coin.java
@@ -7,8 +7,35 @@ import mayflower.*;
  */
 public class Coin extends MoveableAnimatedActor
 {
+    private Animation spin;
+    
     public Coin()
     {
+        String[] files = new String[6];
         
+        //Coin1.png
+        for(int i = 0; i < 6; i++)
+        {
+            files[i] = "img/GoldCoinSprite/Coin" + (i + 1) + ".png";
+        }
+        
+        spin = new Animation(100000000, files);
+        setAnimation(spin);
+    }
+    
+    public void act()
+    {
+        if (isTouching(Cat.class))
+        {
+            Object a = getOneIntersectingObject(Cat.class);
+            Cat c = (Cat)a;
+            
+            World w = getWorld();
+            w.removeObject(this);
+            
+            c.increaseScore(1);
+        }
+        
+        super.act();
     }
 }
