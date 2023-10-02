a72c8e11115b947fdff899a2c65c0e14ee5e7057
bbgolden <144957630+bbgolden@users.noreply.github.com>
2023-10-02 15:57:08 -0500

    Update GameWinScreen.java

--- GameWinScreen.java
+++ GameWinScreen.java
@@ -1,12 +1,11 @@
 import mayflower.*;
 
-public class GameWinScreen extends World{
-
-  public GameWinScreen(){
-
-  }
-
-  public void act(){
-
-  }
+public class GameWinScreen extends World {
+  public GameWinScreen() {
+        setBackground("img/BG/GameWinScreen.png");
+        Mayflower.playMusic("sounds/WinSound.mp3");
+    }
+    
+    public void act() {
+    }
 }
