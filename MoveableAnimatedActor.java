7e05fad84c7c097e7b982d49db6c2159a8b24d17
HKChiuy <144956998+HKChiuy@users.noreply.github.com>
2023-10-02 16:16:14 -0500

    Update MoveableAnimatedActor.java

--- MoveableAnimatedActor.java
+++ MoveableAnimatedActor.java
@@ -9,52 +9,62 @@ public class MoveableAnimatedActor extends AnimatedActor
     private Animation fallLeft;
     private String currentAction;
     private String direction;
-    
+    protected boolean hasShield;
+    private Animation deflect;
+    private boolean isDeflecting;
+
     public MoveableAnimatedActor() {
         direction = "right";
+        hasShield = false;
+        isDeflecting = false;
     }
-    
+
     public void setAnimation(Animation a) {
         super.setAnimation(a);
     }
-    
+
+    public void setDeflectAnimation(Animation ani){
+
+        deflect = ani;
+    }
+
     public void setIdleAnimation(Animation ani) {
         idle = ani;
     }
-    
+
     public void setIdleLeftAnimation(Animation ani) {
         idleLeft = ani;
     }
-    
+
     public void setWalkRightAnimation(Animation ani) {
         walkRight = ani;
     }
-    
+
     public void setWalkLeftAnimation(Animation ani) {
         walkLeft = ani;
     }
-    
+
     public void setFallAnimation(Animation ani) {
         fall = ani;
     }
-    
+
     public void setFallLeftAnimation(Animation ani) {
         fallLeft = ani;
     }
-    
+
     public void act() {
         super.act();
-        
+
         int x = getX();
         int y = getY();
         int w = getWidth();
         int h = getHeight();
         String newAction = null;
-        
+
         if(currentAction == null) {
             newAction = "idle";
         }
-        
+
         if(Mayflower.isKeyDown(Keyboard.KEY_RIGHT) && x < 800 - w) {
             setLocation(x + 1, y);
             if(isBlocked()) {
@@ -62,6 +72,11 @@ public class MoveableAnimatedActor extends AnimatedActor
             }
             newAction = "walkRight";
             direction = "right";
+            
+            if(isDeflecting){
+            
+                newAction = "deflect";
+            }
         }
         else if(Mayflower.isKeyDown(Keyboard.KEY_LEFT) && x > 0) {
             setLocation(x - 1, y);
@@ -72,33 +87,43 @@ public class MoveableAnimatedActor extends AnimatedActor
             direction = "left";
         }
         else if(Mayflower.isKeyDown(Keyboard.KEY_UP) && y > 0) {
-            
-            if (isLadder())
+
+            if (isLadder() || hasShield)
             {
                 setLocation(x, y - 5);
             }
         }
         else if(Mayflower.isKeyDown(Keyboard.KEY_DOWN) && y < 600 - h) {
             setLocation(x, y + 1);
-            
+
             if(isBlocked()) {
                 setLocation(x, y);
             }
-        } else {
+        } 
+        else if (Mayflower.isKeyDown(Keyboard.KEY_SPACE) && hasShield){
+
+            newAction = "deflect";
+            isDeflecting = true;
+        }
+        else 
+        {
             newAction = "idle";
-            if(direction != null && direction.equals("left")) {
+            if(direction != null && direction.equals("left")) 
+            {
                 newAction = "idleLeft";
             }
         }
-        
+
         if(isFalling()) {
             newAction = "fall";
             if(direction != null && direction.equals("left")) {
                 newAction = "fallLeft";
             }
+            
+            if(isDeflecting)
+                newAction = "deflect";
         }
-        
-        
+
         if(newAction != null && !newAction.equals(currentAction)) {
             if(newAction.equals("idle")) {
                 setAnimation(idle);
@@ -112,9 +137,16 @@ public class MoveableAnimatedActor extends AnimatedActor
                 setAnimation(walkRight);
             } else if(newAction.equals("walkLeft")) {
                 setAnimation(walkLeft);
+            } else if (newAction.equals("deflect")){
+                setAnimation(deflect);
             }
             currentAction = newAction;
+            isDeflecting = false;
         }
     }
-    
+
+    public void setHasShield(boolean bool){
+
+        hasShield = true;
+    }
 }
