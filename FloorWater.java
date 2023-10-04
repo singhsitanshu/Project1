a8f87aa6a50c8c20dea81b4af93167859b1fe70c
bbgolden <144957630+bbgolden@users.noreply.github.com>
2023-10-04 16:00:08 -0500

    Update FloorWater.java

--- FloorWater.java
+++ FloorWater.java
@@ -4,7 +4,7 @@ import mayflower.*;
 public class FloorWater extends Actor
 {
     public FloorWater() {
-        MayflowerImage img = new MayflowerImage("img/Tiles/FloorWater.png");
+        MayflowerImage img = new MayflowerImage("img/Tiles/FloorWater2.png");
         img.scale(100, 100);
         setImage(img);
     }
