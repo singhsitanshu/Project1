7e041cce3010217fea5d2525e2353711418fd336
HKChiuy <144956998+HKChiuy@users.noreply.github.com>
2023-10-06 00:55:38 -0500

    Update Missile.java

--- Missile.java
+++ Missile.java
@@ -6,8 +6,13 @@ public class Missile extends Actor{
     private MayflowerImage leftMissile;
     private MayflowerImage rightMissile;
     private int missileSpeed;
+    private Boss boss;
+    private Cat c;
+    private double slope;
+    private double deltaY;
+    private double deltaX;
 
-    public Missile(){
+    public Missile(Boss boss){
 
         leftMissile = new MayflowerImage("img/Object/MissileLeft.png");
         rightMissile = new MayflowerImage("img/Object/MissileRight.png");
@@ -19,6 +24,7 @@ public class Missile extends Actor{
         leftMissile.crop(5, 5 , 65, 35);
 
         missileSpeed = 3;
+        this.boss = boss;
     }
 
     public void act(){
@@ -26,21 +32,39 @@ public class Missile extends Actor{
         double x = getX();
         double y = getY();
 
-        if (!isDeflected)
-            setLocation(x - missileSpeed, y);
-        else 
-            setLocation(x + missileSpeed * 5, y);
-            
         if(isTouching(Cat.class)) {
             
-            Cat c = (Cat) getOneIntersectingObject(Cat.class);
+            c = (Cat) getOneIntersectingObject(Cat.class);
             
-            if (c.getIsDeflecting())
+            if (c.getIsDeflecting()){
+
                 isDeflected = true;
+
+                int catX = c.getX();
+                int catY = c.getY();
+
+                int bossX = boss.getX();
+                int bossY = boss.getY();
+
+                deltaY = bossY - catY;
+                deltaX = bossX - catX;
+
+                rightMissile.rotate(Math.atan(deltaY/deltaX));
+                slope = ((deltaY/deltaX) * 15.0);
+            }
             else
                 c.decreaseLives(1);
         } 
-        
+
+        if (!isDeflected)
+
+            setLocation(x - missileSpeed, y);
+        else {
+                
+            setImage(rightMissile);
+            setLocation(x + missileSpeed * 5, y + slope);
+        }    
+
         if(isTouching(Boss.class)) {
             
             Boss c = (Boss) getOneIntersectingObject(Boss.class);
@@ -48,9 +72,6 @@ public class Missile extends Actor{
             World w = getWorld();
             w.removeObject(this);
         } 
-        
-        if (isDeflected)
-            setImage(rightMissile);
     }
 
     public void setIsDeflected(boolean bool){
